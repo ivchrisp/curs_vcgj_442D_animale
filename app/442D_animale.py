247caf4178f5778f868674240d13d1785e17fd36
Rogoz Andreea <rogozandreea70@yahoo.com>
2023-05-26 22:37:43 +0300

    Am adaugat codul specific animalului meu (pisica) in fisierele 442D_ animale si biblioteca_animale

---
+++ app/442D_animale.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print ('442D_animale')
+
+@app.route("/", methods = ['GET'])
+def index():
+    ret = "<h1></h1>Informatii despre animale 442D</h1>
+    return ret
+
+@app route("/pisica", methods = ['GET'])
+def get_iepure():
+    ret = "<h1>Pisica<h1>"
+    ret += "Culoare: "
+    ret += lib.biblioteca_animale.culoare_pisica()
+    ret += "<br>"
+
+    ret += "Hrana: "
+    ret += lib.biblioteca_animale.hrana_pisica()
+    ret += "<br>"
+
+    ret += "Invelis corp: "
+    ret += lib.biblioteca_animale.invelis_pisica()
+    ret += "<br>"
+
+    return ret
+
+@app.route("/pisica/culoare", methods = ['GET'])
+def ia_culoare_pisica():
+    ret = ""
+    ret += lib.biblioteca_animale.culoare_pisica()
+
+    return ret
+
+@app.route("/pisica/hrana", methods = ['GET'])
+def ia_hrana_pisica():
+    ret = ""
+    ret += lib.biblioteca_animale.hrana_pisica()
+
+    return ret
+
+@app.route("/pisica/invelis", methods = ['GET'])
+def ia_culoare_pisica():
+    ret = ""
+    ret += lib.biblioteca_animale.invelis_pisica()
+
+    return ret
---
+++ app/lib/biblioteca_animale.py
@@ -0,0 +1,8 @@
+def culoare_pisica():
+    return("portocaliu")
+
+def hrana_pisica():
+    return("carnivor")
+
+def invelis_pisica():
+    return("blana")
