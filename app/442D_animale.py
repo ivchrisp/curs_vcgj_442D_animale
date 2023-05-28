107a9fd4f164f078eb65fd7673412f0a004bcf94
Rogoz Andreea <rogozandreea70@yahoo.com>
2023-05-28 19:26:39 +0300

    Am modificat 442D_animale

--- app/442D_animale.py
+++ app/442D_animale.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import lib.biblioteca_animale
 app = Flask(__name__)
 
 print ('442D_animale')
@@ -12,38 +12,35 @@ def index():
 @app route("/pisica", methods = ['GET'])
 def get_pisica():
     ret = "<h1>Pisica<h1>"
-    ret += "Culoare: "
+    ret += "<b>Culoare: </b>"
     ret += lib.biblioteca_animale.culoare_pisica()
     ret += "<br>"
 
-    ret += "Hrana: "
+    ret += "<b>Hrana: </b>"
     ret += lib.biblioteca_animale.hrana_pisica()
     ret += "<br>"
 
-    ret += "Invelis corp: "
+    ret += "<b>Invelis corp: </b>"
     ret += lib.biblioteca_animale.invelis_pisica()
     ret += "<br>"
-
     return ret
 
 @app.route("/pisica/culoare", methods = ['GET'])
 def ia_culoare_pisica():
     ret = ""
     ret += lib.biblioteca_animale.culoare_pisica()
-
     return ret
 
 @app.route("/pisica/hrana", methods = ['GET'])
 def ia_hrana_pisica():
     ret = ""
     ret += lib.biblioteca_animale.hrana_pisica()
-
     return ret
 
 @app.route("/pisica/invelis", methods = ['GET'])
 def ia_culoare_pisica():
     ret = ""
     ret += lib.biblioteca_animale.invelis_pisica()
-
     return ret
+
 app.run (host = "127.0.0.1" , port = 5001)
