f37ab82393631217901ca334be4231cd2bd78fc6
mihnea18 <mihnea.mihaescu@gmail.com>
2023-05-28 18:00:46 +0300

    Modificare Mihnea v6

--- app/tests/test_biblioteca_animale.py
+++ app/tests/test_biblioteca_animale.py
@@ -1,21 +1,21 @@
 import lib.biblioteca_animale as b_animale
 
 def test_culoare_hamster():
-    cul = b_animale.culoare_ornitorinc()
+    cul = b_animale.culoare_hamster()
     if cul == 'bej':
         assert True
     else:
         assert False
 
 def test_hrana_hamster():
-    inf = b_animale.hrana_ornitorinc()
+    inf = b_animale.hrana_hamster()
     if inf == 'ierbivor':
         assert True
     else:
         assert False
 
 def test_invelisul_corpului_hamster():
-    inf = b_animale.invelisul_corpului_ornitorinc()
+    inf = b_animale.invelisul_corpului_hamster()
     if inf == 'blana':
         assert True
     else:
