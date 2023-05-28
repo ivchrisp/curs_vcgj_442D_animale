6a00bbacd5e1f60cc74010798f089d52403942da
paulirimia <paulirimia2707@gmail.com>
2023-05-28 22:04:52 +0300

    Modificare coduri Irimia_Paul-Andrei

---
+++ app/lib/biblioteca_animale.py
@@ -0,0 +1,20 @@
+''' 
+
+------------------------------------ 
+
+NUMELE TAU si GRUPA - oaie 
+
+------------------------------------ 
+
+''' 
+
+def culoare_oaie(): 
+    return("alb") 
+
+def hrana_oaie(): 
+    return("ierbivor") 
+
+def invelisul_corpului_oaie(): 
+    return("lana") 
+
+
