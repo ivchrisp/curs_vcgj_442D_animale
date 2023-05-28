3eaf970c8ce918f4dca56efb2b7ed612bef9fa48
mihnea18 <mihnea.mihaescu@gmail.com>
2023-05-28 17:57:00 +0300

    Modificare Mihnea v5

--- app/tests/test_biblioteca_animale.py
+++ app/tests/test_biblioteca_animale.py
@@ -1,30 +1,21 @@
-'''
- ------------------------------------
-    Stiuca-Mihaescu Mihnea 442D - hamster
- ------------------------------------
-'''
-
 import lib.biblioteca_animale as b_animale
 
-def test_culoare_hamster():  # Defines a test function for culoare_hamster()
-    cul = b_animale.culoare_hamster()
-    if cul == 'bej':  # If the function returns 'bej', the test
-
- passes
+def test_culoare_hamster():
+    cul = b_animale.culoare_ornitorinc()
+    if cul == 'bej':
         assert True
-    else:  # If the function does not return 'bej', the test fails
+    else:
         assert False
 
-# The following two functions are similar to the one above, but for different tests
 def test_hrana_hamster():
-    inf = b_animale.hrana_hamster()
+    inf = b_animale.hrana_ornitorinc()
     if inf == 'ierbivor':
         assert True
     else:
         assert False
 
 def test_invelisul_corpului_hamster():
-    inf = b_animale.invelisul_corpului_hamster()
+    inf = b_animale.invelisul_corpului_ornitorinc()
     if inf == 'blana':
         assert True
     else:
