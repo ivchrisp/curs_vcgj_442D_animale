b64fdc70b89460ff95f128435fc7b80e38cb35f5
Alexandra Ruican <alessia934@yahoo.ro>
2023-05-28 19:36:40 +0300

    Am modificat codul

--- app/442D_animale.py
+++ app/442D_animale.py
@@ -1,49 +1,43 @@
 from flask import Flask
+import lib.biblioteca_animale
 
 app = Flask(__name__)
 
 print ('442D_animale')
 
-@app.route("/", methods = ['GET'])
-def index():
-    ret = "<h1></h1>Informatii despre animale 442D</h1>
-    return ret
-    
-@app route("/iepure", methods = ['GET'])
-def get_iepure():
-    ret = "<h1>Iepure<h1>"
-    ret += "Culoare: "
+@app.route("/iepure", methods = ['GET'])
+def obtine_iepure():
+    ret = "<h1>Iepure:<h1>"
+    ret += "<b>Culoare: </b>"
     ret += lib.biblioteca_animale.culoare_iepure()
     ret += "<br>"
     
-    ret += "Hrana: "
+    ret += "<b>Hrana: </b>"
     ret += lib.biblioteca_animale.hrana_iepure()
     ret += "<br>"
     
-    ret += "Invelis corp: "
+    ret += "<b>Invelis corp: </b>"
     ret += lib.biblioteca_animale.invelis_iepure()
     ret += "<br>"
     
     return ret
     
 @app.route("/iepure/culoare", methods = ['GET'])
-def ia_culoare_iepure():
+def obtine_culoare_iepure():
     ret = ""
     ret += lib.biblioteca_animale.culoare_iepure()
-    
     return ret
     
 @app.route("/iepure/hrana", methods = ['GET'])
-def ia_hrana_iepure():
+def obtine_hrana_iepure():
     ret = ""
     ret += lib.biblioteca_animale.hrana_iepure()
-    
     return ret
     
 @app.route("/iepure/invelis", methods = ['GET'])
-def ia_culoare_iepure():
+def obtine_invelis_iepure():
     ret = ""
     ret += lib.biblioteca_animale.invelis_iepure()
-    
     return ret
 
+app.run(host = "127.0.0.1", port = 5001)
