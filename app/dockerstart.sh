3f76d95b7c7ff00e552f9360b1ab8f950c6854d3
romeofantastick <alexandru.ion00@stud.etti.upb.ro>
2023-05-29 12:40:48 +0300

    Adaugare fisiere necesare Docker

---
+++ app/dockerstart.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo `pwd`
+#echo "changing directory to app:"
+#cd ./app
+#echo `pwd`
+echo "Activare venv:"
+#source ../.vanv/bin/activate
+. ../.venv/bin/activate
+echo `pwd`
+echo "Configurare variabila mediu FLASK_APP"
+export FLASK_APP=442D_animale
+echo "Start server:"
+exec flask run -h 0.0.0.0 -p 5020 --reload
