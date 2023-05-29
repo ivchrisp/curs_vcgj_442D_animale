8a6ca4f95c58cd82da466bf76c9e2ec39b25ada6
xcatalin_capra <catalintheodor20@yahoo.com>
2023-05-29 15:25:20 +0300

    Modificare coduri xcatalin

--- app/test/test_biblioteca_animale.py
+++ app/test/test_biblioteca_animale.py
@@ -1,20 +1,20 @@
 import lib.biblioteca_animale as b_animale
 
-def test_culoare_urs():
-    cul = b_animale.culoare_urs()
-    if cul == 'brun':
+def test_culoare_capra():
+    cul = b_animale.culoare_capra()
+    if cul == 'alb':
         assert True
     else:
         assert False
-def test_hrana_urs():
-    inf = b_animale.hrana_urs()
-    if inf == 'omnivor':
+def test_hrana_capra():
+    inf = b_animale.hrana_capra()
+    if inf == 'ierbivor':
         assert True
     else:
         assert False
 
-def test_invelisul_corpului_urs():
-    inf = b_animale.invelisul_corpului_urs()
+def test_invelisul_corpului_capra():
+    inf = b_animale.invelisul_corpului_capra()
     if inf == 'blana':
         assert True
     else:
